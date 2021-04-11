81343f7b66970d774260020885dca4d8ddd129d9
BogdanLyopa <bogdanlyopa@gmail.com>
2021-04-11 14:12:19 +0300

    moved files from previous homework

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import Form from './Components/Form';
+import ContactsList from './Components/ContactsList';
+import Filter from './Components/Filter';
+
+import './styles.css';
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  handleAddContact = contact => {
+    const newContact = {
+      id: uuidv4(),
+      ...contact,
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, newContact],
+    }));
+  };
+
+  handleRemoveContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  handleCheckUniqueContact = name => {
+    const { contacts } = this.state;
+    const check = contacts.find(contact => {
+      return contact.name === name;
+    });
+    if (check) {
+      alert('Contact is already exist');
+      return check;
+    }
+  };
+
+  handleFilterChange = filter => {
+    this.setState({ filter });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  render() {
+    const visibleContacts = this.getVisibleContacts();
+    const { filter } = this.state;
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <Form
+          onSubmit={this.handleAddContact}
+          onCheckUnique={this.handleCheckUniqueContact}
+        />
+        {this.state.contacts.length > 0 && (
+          <Filter filter={filter} onChange={this.handleFilterChange} />
+        )}
+        <ContactsList
+          contacts={visibleContacts}
+          onRemove={this.handleRemoveContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
